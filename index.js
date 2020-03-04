5783ce25e488c8cd2336807b085dff4a8b3b58ee
wbrink <will.brink55@gmail.com>
2020-03-03 23:32:42 -0800

    setting up project

---
+++ index.js
@@ -0,0 +1,21 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+
+
+
+const api = require("./utils/api");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const questions = [
+  "What is the Title"
+];
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() {
+
+}
+
+init();
